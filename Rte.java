5eee656263c5ae8c4b6de9f097dbe9041c718f94
Abhishek singh <abhisheks0123singh@gmail.com>
2025-08-03 23:51:45 +0530

    Add files via upload

--- Rte.java
+++ Rte.java
@@ -1,8 +1,27 @@
 public class Rte {
-    public static void main(String[] args) {
-        int num=12345;
-        int k=2;
-        
+     public static void main(String[] args) {
+        int num = 72456;
+        int k = 3;
+        // output = 45672
+        int copy = num;
+
+        int count = 0;
+        while (num >= 10) {
+            count++;
+            num = num / 10;
+        }
+        num = copy;
+        System.out.println(count);
+        System.out.println(num);
+
+        for (int i = 0; i < k; i++) {
+            int lastDigit = num % 10;
+            num = lastDigit * (int) Math.pow(10, count) + num / 10;
+            // 40000
+            // 5123
+        }
+        System.out.println(num);
     }
-    
 }
+        
+    
